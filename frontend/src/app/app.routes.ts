150880b753dc9b37ddf0aac596f43e7477d4de52
JsRkll132 <jose.apaza2@unmsm.edu.pe>
2025-11-05 23:52:18 -0500

    add things frontend

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Sales } from './components/sales/sales';
+import { Products } from './components/products/products';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'products',component : Products},
+    {path:'sales',component : Sales}
+];
